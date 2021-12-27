8eef5f90ca6a2a20d5dc512475533a1bf8b8c6d9
tasszz2k <tasszz2k@gmail.com>
2021-12-27 17:16:25 +0700

    update code

--- gorm_demo.go
+++ gorm_demo.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 	"os"
 )
 
 func main() {
+
+	// Connect to MySQL
+
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 
@@ -23,4 +27,41 @@ func main() {
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	fmt.Println(db, err)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// Insert new Note
+	newNote := Note{
+		Title:   "My First Note",
+		Content: "This is my first note",
+	}
+	if err := db.Create(&newNote).Error; err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println(newNote)
+
+	// Select all Notes where id > 1
+	var notes []Note
+	if err := db.Where("id > ?", 1).Find(&notes).Error; err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println(notes)
+
+}
+
+/**
+  id      INT AUTO_INCREMENT,
+  title   VARCHAR(100) NOT NULL,
+  content TEXT         NULL,
+  image   JSON         NULL,
+*/
+type Note struct {
+	Id      int    `json:"id,ommitempty" gorm:"column:id;primary_key;AUTO_INCREMENT"`
+	Title   string `json:"title" gorm:"column:title"`
+	Content string `json:"content" gorm:"column:content"`
+}
+
+func (Note) TableName() string {
+	return "notes"
 }
