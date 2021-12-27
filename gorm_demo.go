f7235a556648614c59496b4af2bc61afbaa7629e
tasszz2k <tasszz2k@gmail.com>
2021-12-27 18:09:06 +0700

    update ...

--- gorm_demo.go
+++ gorm_demo.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -26,10 +27,21 @@ func main() {
 	fmt.Println(dsn)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	sqlDB, err := db.DB()
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
 	fmt.Println(db, err)
 	if err != nil {
 		log.Println(err)
 	}
+
+	// ============================================================
 
 	// Insert new Note
 	newNote := Note{
